e52d90ac76fc229716d3bd3b20e47d451dbb06af
dvzhang <dvzhangtz@sina.com>
2022-12-31 00:37:25 -0800

    12.30 commit

--- compile/deploy.js
+++ compile/deploy.js
@@ -7,26 +7,44 @@ const HdWalletProvider = require('truffle-hdwallet-provider')
 const contractPath = path.resolve(__dirname, '../src/compiled/CourseList.json')
 const {interface, bytecode} = require(contractPath)
 
-const mnemonic = "brown fine require between stereo man knife cup kidney marine offer alter"; // 12 word mnemonic
+require('dotenv').config();
+const mnemonic = process.env.MNEMONIC;
+const infuraId = process.env.INFURA_ID
+// const contractAddress = process.env.CONTRACT_ADDRESS
+
 const provider = new HdWalletProvider(
     mnemonic,
-    // "https://sepolia.infura.io/v3/dfb9707563294927a29279a9d0491258"
-    "https://sepolia.infura.io/v3/5043a4985de24c0ab57fd25f3e444504"
+    // "https://sepolia.infura.io/v3/" + infuraId
+    'https://goerli.infura.io/v3/' + infuraId
+    // "https://mainnet.infura.io/v3/" + infuraId
 )
+
 const web3 = new Web3(provider);
 
 (async()=>{
     console.log('自执行')
     const accounts = await web3.eth.getAccounts()
+    // console.log(accounts.length)
+    console.log(web3.eth.getBalance(accounts[0]))
+
+    // for(i=1; i<accounts.length; i++){
+    //     console.log(accounts[i])
+    // }
     console.log('合约部署的账号：', accounts)
     // await
     console.time('合约部署消耗时间')
+    console.log("done")
     const result = await new web3.eth.Contract(JSON.parse(interface))
                       .deploy({data: bytecode})
                       .send({
                         from:accounts[0],
-                        gas:'5000000'
+                        gas:'30000000'
                       })
+                      .on("error", function (error) {
+                        console.log("error", error);
+                      })
+    console.log("done")
+
     console.timeEnd('合约部署消耗时间')
     const contractAddress = result.options.address
     console.log('合约部署成功', contractAddress)
